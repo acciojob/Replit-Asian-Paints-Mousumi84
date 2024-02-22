0dec363bb589f91f88c0165be58df89761ce30a5
Mousumi Dsa <58068282+Mousumi84@users.noreply.github.com>
2024-02-23 03:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+let grid=document.querySelector("#grid");
+let form=document.querySelector("form");
+let previousSelectedCell=null;
+
+form.addEventListener("submit", (eveObj) => {
+	eveObj.preventDefault();
+
+	let inputvalue=form[0].value;
+	let colorvalue=form[1].value;
+
+	console.log(inputvalue, colorvalue);
+	let cell=document.getElementById(inputvalue);
+	cell.style.background= colorvalue;
+
+	if(previousSelectedCell)
+	{
+		previousSelectedCell.style.background= "transparent";
+	}
+	previousSelectedCell=cell;
+
+});
